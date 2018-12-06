207bae4dff582599f291db77f20002c7709044fc
Maciek <maciej.szpakowski@avtechcorp.com>
2018-12-06 11:29:08 -0600

    Testing alert

--- GitWorkshop/Controller/ViewController.swift
+++ GitWorkshop/Controller/ViewController.swift
@@ -21,6 +21,13 @@ class ViewController: UIViewController {
     // MARK: - Lazy Private Properties
     
     lazy private var textBoxView: TextBoxView = {
+	let alert = UIAlertController(title: "Did you bring your towel?", message: "It's recommended you bring your towel before continuing.", preferredStyle: .alert)
+
+alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+
+self.present(alert, animated: true)
+	
         let newTextBox = TextBoxView(title: "Pass", // TODO: - Change title parameter to pass
                                      description: "Please enter a title in the text field below",
                                      placeholderText: "Enter title")
